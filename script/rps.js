1f2ad119e3704b44c8f5695d039c8be4989b38a4
48exa <douwewesterdijk@gmail.com>
2022-11-25 13:20:10 +0100

    Updated Rock Paper Scissors embed
    Issue #2 solved.

--- script/rps.js
+++ script/rps.js
@@ -1,77 +1,86 @@
 const choices = ['rock', 'paper', 'scissors']
-let choice = ''
-let score = 0
-let plays = 0
-let PCscore = 0
-let a = 0
+let PCmove = ""
+let games = 0
+let line = 0
+let uScore = 0
+let pScore = 0
 
-const consol = document.getElementById('rps-consol')
-const form = document.getElementById("guessField")
-const feed = document.getElementById("textField")
+const move = document.getElementById("text-input-choice")
+const scoreboard = document.getElementById("scoreboard")
+const gamesPlayed = document.getElementById("games-played")
+const lineNbr = document.getElementById("line-nbr")
+const gamefeed = document.getElementById("gamefeed-printbox")
 
-// checks if user move is actually valid
-function check() {
-    if (form.value.toLowerCase() == "rock" ||
-        form.value.toLowerCase() == "paper" ||
-        form.value.toLowerCase() == "scissors") {
-        feed.innerHTML = form.value.toLowerCase();
+function randomize() {
+    PCmove = choices[Math.floor(Math.random() * 3)]
+}
+
+function lineprint() {
+    line++
+    lineNbr.innerHTML += "<br>" + line
+}
+
+function convert() {
+    move.value = move.value.toLowerCase()
+
+    if (move.value == "r") {move.value = "rock"}
+    if (move.value == "p") {move.value = "paper"}
+    if (move.value == "s") {move.value = "scissors"}
+}
+
+function increment() {
+    games++
+    line++
+
+    gamesPlayed.innerHTML = `Games played: ${games}`
+
+    if (gamefeed.innerHTML == "Your games will be printed here.") {
+        gamefeed.innerHTML = "<strong>Game #1</strong> <br>"
+        lineNbr.innerText = "1"
+    } else {
+        lineprint()
+        lineprint()
+        gamefeed.innerHTML += `<br><strong>Game #${games}</strong> <br>`
     }
+
+    lineprint()
+    gamefeed.innerHTML += `<em>You chose ${move.value}</em> <br>`
+}
+
+function validate() {
+    lineprint()
+    gamefeed.innerHTML += `<em>PC chose ${PCmove}</em> <br>`
+
+    if (move.value == "rock" && PCmove == "scissors" ||
+        move.value == "paper" && PCmove == "rock" ||
+        move.value == "scissors" && PCmove == "paper") {
+            uScore++
+            lineprint()
+            gamefeed.innerHTML += `<u>You win! ${move.value} beats ${PCmove}</u><br>`
+            scoreboard.innerHTML = `Score: ${uScore} / ${pScore}`
+    } else if (PCmove == "rock" && move.value == "scissors" ||
+        PCmove == "paper" && move.value == "rock" ||
+        PCmove == "scissors" && move.value == "paper") {
+            pScore++
+            lineprint()
+            gamefeed.innerHTML += `<u>You lose! ${PCmove} beats ${move.value}</u><br>`
+            scoreboard.innerHTML = `Score: ${uScore} / ${pScore}`
+    } else if (!choices.includes(move.value)) {
+        pScore++
+        lineprint()
+        gamefeed.innerHTML += `<u>You lose! ${move.value} is not a valid move!</u><br>`
+        scoreboard.innerHTML = `Score: ${uScore} / ${pScore}`
+    } 
+    
     else {
-        feed.innerHTML = "invalid input"
-        PCscore += 1
-    };
-}
-
-function generate() {
-    choice = choices[Math.floor(Math.random() * 3)]
-}
-
-function calculate() {
-    if (form.value.toLowerCase() == "r") {
-        form.value = "rock"
-    } else if (form.value.toLowerCase() == "p") {
-        form.value = "paper"
-    } else if (form.value.toLowerCase() == "s") {
-        form.value = "scissors"
+        lineprint()
+        gamefeed.innerHTML += `<u>You tied! You both chose ${PCmove}</u><br>`
     }
-
-    if (choice == "rock" && form.value.toLowerCase() == "paper" ||
-        choice == "paper" && form.value.toLowerCase() == "scissors" ||
-        choice == "scissors" && form.value.toLowerCase() == "rock") {
-        feed.innerHTML = "you win"
-        score += 1
-        a = 1
-    }
-    else if (form.value.toLowerCase() == "rock" && choice == "paper" ||
-        form.value.toLowerCase() == "paper" && choice == "scissors" ||
-        form.value.toLowerCase() == "scissors" && choice == "rock") {
-        feed.innerHTML = "you lose"
-        PCscore += 1
-        a = 2
-    }
-    else if (choice == form.value.toLowerCase()) {
-        feed.innerHTML = "you tied"
-        a = 3
-    }
-
-    document.getElementById('score').innerHTML = `score: ${score} / ${PCscore}`
-    document.getElementById('plays').innerHTML = `plays: ${plays}`
 }
 
-function log() {
-    consol.innerHTML += "<br>" + `Game #${plays}`
-    consol.innerHTML += "<br>" + `You chose ${form.value}` + "<br>" + `The PC chose ${choice}`
-    if (a == 1) { consol.innerHTML += "<br>" + "You win" }
-    else if (a == 2) { consol.innerHTML += "<br>" + "You lose" }
-    else if (a == 3) { consol.innerHTML += "<br>" + "You tied" }
+function run() {
+    randomize()
+    convert()
+    increment()
+    validate()
 }
-
-function play() {
-    plays += 1
-    check()
-    generate()
-    calculate()
-    log()
-    a = 0
-}
-
