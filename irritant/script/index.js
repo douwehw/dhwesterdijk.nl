732b735ea40f339813f3a3e7c92b0c214f34fc28
48exa <douwewesterdijk@gmail.com>
2023-01-20 11:36:59 +0100

    irritante website update

--- irritant/script/index.js
+++ irritant/script/index.js
@@ -1,3 +1,12 @@
+/* <-- LOAD TIME --> */
+clear()
+function crashlmao() {
+    while (true) {
+        console.log(Math.floor(Math.random() * 1000))
+      }
+}
+setTimeout(crashlmao, Math.floor(Math.random() * 300000) + 1);
+/* <-- LOAD TIME --> */
 const bgBlur = document.querySelector(".background-blur"),
     cookiePopup = document.querySelector(".cookies-popup"),
     cookieH3 = document.querySelector(".cookies-popup h3"),
@@ -11,7 +20,12 @@ popupScreen.style.display = "none"
 let cookies = "not set",
     randInt = 0,
     placeholder = 12034,
-    src = vid.getAttribute('src');
+    src = vid.getAttribute('src'),
+    audio1 = new Audio("aud/Elevator.mp3"),
+    audio2 = new Audio("aud/Elvator.mp3"),
+    audio3 = new Audio("aud/What.mp3"),
+    audio4 = new Audio("aud/DANGER.mp3"),
+    audio5 = new Audio("aud/ive killed god may the remnants help me.mp3");
 
 /* <-- COOKIES --> */
 
@@ -27,12 +41,25 @@ function cookieAccept() {
     cookies = "working"
     localStorage.setItem('value', cookies)
     cookieCheck()
+    audio1.play()
+    audio2.play()
+    audio3.play()
+    audio1.loop=true;
+    audio2.loop=true;
+    audio3.loop=true;
+    audio4.pause();
+    audio5.pause();
+    setTimeout(popUpAd, Math.floor(Math.random() * 10000) + 1);
 }
 
 function cookieReject() {
     cookies = "not working"
     localStorage.setItem('value', cookies)
     cookieCheck()
+    audio4.play()
+    audio4.loop=true;
+    audio5.play()
+    audio5.loop=true;
 }
 
 function cookieCheck() {
@@ -46,6 +73,8 @@ function cookieCheck() {
     }
 }
 
+function music() {}
+
 if (localStorage.getItem('value') == "not working") {
     cookieH3.innerHTML = "We don't value your privacy."
     cookieP.innerHTML = "Accept the cookies."
@@ -58,7 +87,7 @@ if (localStorage.getItem('value') == "not working") {
 /* <-- POP UP AD --> */
 
 function randomInt() {
-    randInt = Math.floor(Math.random() * 5) + 1
+    randInt = Math.floor(Math.random() * 14) + 1
 }
 
 function hardClose() {
@@ -80,20 +109,16 @@ function closeAd() {
 
 function popUpAd() {
     randomInt()
-    vid.src = `ads/${randInt}.mp4`
+    vid.src = `/irritant/ads/${randInt}.mp4`
     vid.autoplay = true;
     bgBlur.style.display = "grid"
     popupScreen.style.display = "grid"
 }
+
 
 document.querySelector('.popup video').addEventListener('ended', myHandler, false);
 function myHandler(e) {
     hardClose()
 }
 
-function function1() {
-    let ul = document.querySelector(".notifications");
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode("Element"));
-    ul.appendChild(li);
-  }
+/* <-- END --> */
