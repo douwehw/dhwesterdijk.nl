9d718c461a8a4fba4cff412ec553a2b7884bfe0e
48exa <97523839+48exa@users.noreply.github.com>
2023-05-08 12:27:39 +0200

    Added contact page

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,101 +1,101 @@
 import { FiMail } from "react-icons/fi";
 import { SiGithub, SiDiscord } from "react-icons/si";
 import Flag from "react-world-flags";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function Navbar({ SetSiteLang, siteLang }) {
-  const NavIcons = () => {
-    return (
-      <div className="md:grid hidden float-right m-[18px]">
-        <ul className="grid grid-cols-3 gap-3">
-          <li>
-            <a href="https://github.com/48exa" target="_blank">
-              <SiGithub className="w-6 h-6 transition ease-in-out delay-[10] hover:text-[#9aaddf] cursor-pointer" />
-            </a>
-          </li>
-          <a href="https://discord.com/users/350306353702764554" target="_blank">
-            <li>
-              <SiDiscord className="w-6 h-6 transition ease-in-out delay-[10] hover:text-[#9aaddf] cursor-pointer" />
-            </li>
-          </a>
-          <a href="mailto: westerdijk@protonmail.com" target="_blank">
-            <li>
-              <FiMail className="w-6 h-6 transition ease-in-out delay-[10] hover:text-[#9aaddf] cursor-pointer" />
-            </li>
-          </a>
-        </ul>
-      </div>
-    );
-  };
+	const NavIcons = () => {
+		return (
+			<div className="md:grid hidden float-right m-[18px]">
+				<ul className="grid grid-cols-3 gap-3">
+					<li>
+						<a href="https://github.com/48exa" target="_blank">
+							<SiGithub className="w-6 h-6 transition ease-in-out delay-[10] hover:text-[#9aaddf] cursor-pointer" />
+						</a>
+					</li>
+					<a href="https://discord.com/users/350306353702764554" target="_blank">
+						<li>
+							<SiDiscord className="w-6 h-6 transition ease-in-out delay-[10] hover:text-[#9aaddf] cursor-pointer" />
+						</li>
+					</a>
+					<Link to="/contact">
+						<li>
+							<FiMail className="w-6 h-6 transition ease-in-out delay-[10] hover:text-[#9aaddf] cursor-pointer" />
+						</li>
+					</Link>
+				</ul>
+			</div>
+		);
+	};
 
-  const NavButton = ({ children }) => {
-    return (
-      <li className="transition ease-in-out delay-[10] rounded-lg text-gray-300 hover:text-white hover:bg-[#2a2f3d] p-[6px] cursor-pointer text-center">
-        {children}
-      </li>
-    );
-  };
+	const NavButton = ({ children }) => {
+		return (
+			<li className="transition ease-in-out delay-[10] rounded-lg text-gray-300 hover:text-white hover:bg-[#2a2f3d] p-[6px] cursor-pointer text-center">
+				{children}
+			</li>
+		);
+	};
 
-  const TranslateFlagButton = () => {
-    return (
-      <button
-        className="transition ease-in-out delay-[10] rounded-lg hover:bg-[#2a2f3d] p-2 m-1 cursor-pointer w-10 h-10"
-        onClick={() => {
-          if (siteLang === "NL") {
-            localStorage.setItem("lang", "GB");
-          } else {
-            localStorage.setItem("lang", "NL");
-          }
-          SetSiteLang(localStorage.getItem("lang"));
-        }}
-      >
-        <Flag code={siteLang} />
-      </button>
-    );
-  };
+	const TranslateFlagButton = () => {
+		return (
+			<button
+				className="transition ease-in-out delay-[10] rounded-lg hover:bg-[#2a2f3d] p-2 m-1 cursor-pointer w-10 h-10"
+				onClick={() => {
+					if (siteLang === "NL") {
+						localStorage.setItem("lang", "GB");
+					} else {
+						localStorage.setItem("lang", "NL");
+					}
+					SetSiteLang(localStorage.getItem("lang"));
+				}}
+			>
+				<Flag code={siteLang} />
+			</button>
+		);
+	};
 
-  const NavPages = () => {
-    return (
-      <div className="md:float-left flex items-center h-[60px]">
-        <ul className="flex flex-row">
-          <TranslateFlagButton />
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? "bg-[#2a2f3d] flex items-center rounded-lg h-11 mt-[2px]" : "mt-2"
-            }
-          >
-            <NavButton>{siteLang === "NL" ? "Over Mij" : "About Me"}</NavButton>
-          </NavLink>
-          <p className="mt-3 p-1 text-opacity-40 text-white hidden md:block">•</p>
-          <NavLink
-            to="/projects"
-            className={({ isActive }) =>
-              isActive ? "bg-[#2a2f3d] flex items-center rounded-lg h-11 mt-[2px]" : "mt-2"
-            }
-          >
-            <NavButton>{siteLang === "NL" ? "Projecten" : "Projects"}</NavButton>
-          </NavLink>
-          <p className="mt-3 p-1 text-opacity-40 text-white hidden md:block">•</p>
-          <NavLink
-            to="/experience"
-            className={({ isActive }) =>
-              isActive ? "bg-[#2a2f3d] flex items-center rounded-lg h-11 mt-[2px]" : "mt-2 hover:bg-transparent"
-            }
-          >
-            <NavButton>{siteLang === "NL" ? "Ervaring met coderen" : "Coding experience"}</NavButton>
-          </NavLink>
-        </ul>
-      </div>
-    );
-  };
+	const NavPages = () => {
+		return (
+			<div className="md:float-left flex items-center h-[60px]">
+				<ul className="flex flex-row">
+					<TranslateFlagButton />
+					<NavLink
+						to="/"
+						className={({ isActive }) =>
+							isActive ? "bg-[#2a2f3d] flex items-center rounded-lg h-11 mt-[2px]" : "mt-2"
+						}
+					>
+						<NavButton>{siteLang === "NL" ? "Over Mij" : "About Me"}</NavButton>
+					</NavLink>
+					<p className="mt-3 p-1 text-opacity-40 text-white hidden md:block">•</p>
+					<NavLink
+						to="/projects"
+						className={({ isActive }) =>
+							isActive ? "bg-[#2a2f3d] flex items-center rounded-lg h-11 mt-[2px]" : "mt-2"
+						}
+					>
+						<NavButton>{siteLang === "NL" ? "Projecten" : "Projects"}</NavButton>
+					</NavLink>
+					<p className="mt-3 p-1 text-opacity-40 text-white hidden md:block">•</p>
+					<NavLink
+						to="/experience"
+						className={({ isActive }) =>
+							isActive ? "bg-[#2a2f3d] flex items-center rounded-lg h-11 mt-[2px]" : "mt-2 hover:bg-transparent"
+						}
+					>
+						<NavButton>{siteLang === "NL" ? "Ervaring met coderen" : "Coding experience"}</NavButton>
+					</NavLink>
+				</ul>
+			</div>
+		);
+	};
 
-  return (
-    <nav className="bg-[#3f4d5a5d] z-50 fixed m-3 backdrop-blur-[6px] rounded-md border-solid border-spacing-4 border border-[#425d72] w-[370px] md:w-[70%]">
-      <NavPages />
-      <NavIcons />
-    </nav>
-  );
+	return (
+		<nav className="bg-[#3f4d5a5d] z-50 fixed m-3 backdrop-blur-[6px] rounded-md border-solid border-spacing-4 border border-[#425d72] w-[370px] md:w-[70%]">
+			<NavPages />
+			<NavIcons />
+		</nav>
+	);
 }
 
 export default Navbar;
--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,38 +1,95 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import SiteContainer from "../components/SiteContainer";
 
 function Contact(siteLang) {
-  const ContactNL = () => {
-    return (
-      <SiteContainer>
-        <>
-          <h1 className="text-white font-bold text-5xl mb-3 mt-8">Hmm... Hier gaat iets fout... 🤔</h1>
-          <p className="text-gray-300 mb-6 text-lg">Het blijkt dat de pagina die je zoekt zich hier niet bevindt...</p>
-          <Link to={sessionStorage.getItem("latestPage")}>
-            <p className="text-gray-300 mb-6 text-lg underline italic">Neem me weer terug graag!</p>
-          </Link>
-        </>
-      </SiteContainer>
-    );
-  };
+	const ContactNL = () => {
+		const [mailTitle, setmailTitle] = useState("");
+		const [mailBody, setmailBody] = useState("");
+		return (
+			<SiteContainer>
+				<>
+					<div className="md:col-span-2 row-span-3 bg-opacity-50 bg-white dark:bg-white/5 rounded-md p-4 border border-zinc-800/50">
+						<h1 className="font-bold text-sm dark:text-slate-500 mb-1">ONDERWERP</h1>
+						<input
+							placeholder="Hoe gaat het?"
+							type="text"
+							onChange={() => {
+								setmailTitle(event.target.value);
+							}}
+							className="w-full p-2 mb-4 rounded-md bg-slate-300/50 dark:bg-slate-200/5 text-sm placeholder:text-gray-600 dark:placeholder:text-slate-200/20"
+						/>
 
-  const ContactEN = () => {
-    return (
-      <SiteContainer>
-        <>
-          <h1 className="text-white font-bold text-5xl mb-3 mt-8">Hmm... Something is wrong... 🤔</h1>
-          <p className="text-gray-300 mb-6 text-lg">It appears the page you're looking for isn't here...</p>
-          <Link to={sessionStorage.getItem("latestPage")}>
-            <p className="text-gray-300 mb-6 text-lg underline italic">Take me back please!</p>
-          </Link>
-        </>
-      </SiteContainer>
-    );
-  };
+						<h1 className="font-bold text-sm dark:text-slate-500 mb-1">BERICHT</h1>
+						<textarea
+							onChange={() => {
+								setmailBody(event.target.value);
+							}}
+							placeholder="Ik zou je graag spreken."
+							className="w-full p-2 h-36 mb-4 rounded-md bg-slate-300/50 dark:bg-slate-200/5 text-sm placeholder:text-gray-600 dark:placeholder:text-slate-200/20"
+						/>
 
-  if (siteLang.siteLang === "NL") return <ContactNL />;
-  else return <ContactEN />;
+						<div className="w-full flex flex-row justify-between items-center">
+							<p className="text-gray-900 dark:text-gray-300 text-sm"></p>
+
+							<a
+								target="_blank"
+								href={`mailto:westerdijk@pm.me?subject=${mailTitle}&body=${mailBody}`}
+								className="border border-gray-800 hover:bg-gray-200 dark:border-indigo-600/80 dark:bg-indigo-600/70 dark:hover:bg-indigo-500/70 flex flex-row items-center justify-center rounded-full px-5 py-2 text-sm font-medium transition-colors duration-75"
+							>
+								<span className="mt-[2px]">Stuur</span>
+							</a>
+						</div>
+					</div>
+				</>
+			</SiteContainer>
+		);
+	};
+
+	const ContactEN = () => {
+		const [mailTitle, setmailTitle] = useState("");
+		const [mailBody, setmailBody] = useState("");
+		return (
+			<SiteContainer>
+				<>
+					<div className="md:col-span-2 row-span-3 bg-opacity-50 bg-white dark:bg-white/5 rounded-md p-4 border border-zinc-800/50">
+						<h1 className="font-bold text-sm dark:text-slate-500 mb-1">SUBJECT</h1>
+						<input
+							placeholder="How's it going?"
+							type="text"
+							onChange={() => {
+								setmailTitle(event.target.value);
+							}}
+							className="w-full p-2 mb-4 rounded-md bg-slate-300/50 dark:bg-slate-200/5 text-sm placeholder:text-gray-600 dark:placeholder:text-slate-200/20"
+						/>
+
+						<h1 className="font-bold text-sm dark:text-slate-500 mb-1">BODY</h1>
+						<textarea
+							onChange={() => {
+								setmailBody(event.target.value);
+							}}
+							placeholder="Tell me about your day."
+							className="w-full p-2 h-36 mb-4 rounded-md bg-slate-300/50 dark:bg-slate-200/5 text-sm placeholder:text-gray-600 dark:placeholder:text-slate-200/20"
+						/>
+
+						<div className="w-full flex flex-row justify-between items-center">
+							<p className="text-gray-900 dark:text-gray-300 text-sm"></p>
+
+							<a
+								target="_blank"
+								href={`mailto:westerdijk@pm.me?subject=${mailTitle}&body=${mailBody}`}
+								className="border border-gray-800 hover:bg-gray-200 dark:border-indigo-600/80 dark:bg-indigo-600/70 dark:hover:bg-indigo-500/70 flex flex-row items-center justify-center rounded-full px-5 py-2 text-sm font-medium transition-colors duration-75"
+							>
+								<span className="mt-[2px]">Send</span>
+							</a>
+						</div>
+					</div>
+				</>
+			</SiteContainer>
+		);
+	};
+
+	if (siteLang.siteLang === "NL") return <ContactNL />;
+	else return <ContactEN />;
 }
 
 export default Contact;
